03b55821da3e811da337dc0fcf6b1d4ff9f5dd28
Krupal <sanchaniya2004krupal@gmail.com>
2024-04-13 22:42:55 +0530

    Some code are remove in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,64 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 5, 99, 181),
 );
 
+/*class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.scrim,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        snackBarTheme: const SnackBarThemeData().copyWith(
+          backgroundColor: kDarkColorScheme.shadow,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.outlineVariant,
+              foregroundColor: kDarkColorScheme.onSurface),
+        ),
+      ),
+      theme: ThemeData(
+        colorScheme: kColorSeed,
+        primaryColor: Colors.deepPurple,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorSeed.onPrimaryContainer,
+          foregroundColor: kColorSeed.primaryContainer,
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ).copyWith(
+          titleLarge: const TextStyle(
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          bodyMedium: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorSeed.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSeed.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorSeed.primaryContainer),
+        ),
+        snackBarTheme: const SnackBarThemeData()
+            .copyWith(backgroundColor: kColorSeed.onPrimaryContainer),
+      ),
+      home: const Expenses(),
+    );
+  }
+}*/
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
